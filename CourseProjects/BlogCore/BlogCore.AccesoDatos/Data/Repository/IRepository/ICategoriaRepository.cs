151b137a83594c979272c67f9b3a4688485f8b7a
D-Madev <3matias.sm@gmail.com>
2025-09-22 09:36:45 -0300

    feat(respository): Agrego dependencia necesaria y creo funcion para listar categorias

--- CourseProjects/BlogCore/BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
+++ CourseProjects/BlogCore/BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoriaRepository.cs
@@ -5,11 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using BlogCore.Data;
 using BlogCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlogCore.AccesoDatos.Data.Repository.IRepository
 {
     public interface ICategoriaRepository : IRepository<Categoria>
     {
         void Update(Categoria categoria);
+
+        IEnumerable<SelectListItem> GetListaCategorias();
     }
 }   
