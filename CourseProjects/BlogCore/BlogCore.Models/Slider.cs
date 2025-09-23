970e675bfd1f2738bafb8427074ba44f366e096a
D-Madev <3matias.sm@gmail.com>
2025-09-23 12:30:05 -0300

    feat(Models): creo entidad sliders

---
+++ CourseProjects/BlogCore/BlogCore.Models/Slider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models
+{
+    public class Slider
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [NotNull]
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [NotNull]
+        public bool estado { get; set; } = false;
+
+        [NotNull]
+        [Required(ErrorMessage = "La imagen es obligatoria.")]
+        public string UrlImage { get; set; } = string.Empty;
+    }
+}
