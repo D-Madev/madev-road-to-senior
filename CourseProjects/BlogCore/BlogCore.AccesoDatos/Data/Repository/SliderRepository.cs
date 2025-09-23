d0b65b2fc14cbd45629e4931717f0409e8eccc84
D-Madev <3matias.sm@gmail.com>
2025-09-23 12:39:17 -0300

    feat(Repository): agrego Interfaz e implemento

---
+++ CourseProjects/BlogCore/BlogCore.AccesoDatos/Data/Repository/SliderRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Data;
+using BlogCore.Models;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    internal class SliderRepository : Repository<Slider>, ISliderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public SliderRepository(ApplicationDbContext context) : base(context) => _context = context;
+        public void Update(Slider slider)
+        {
+            var obj = _context.Sliders.FirstOrDefault(s => s.Id == slider.Id);
+            if (obj == null) return;
+
+            obj.Nombre = slider.Nombre;
+            obj.estado = slider.estado;
+            obj.UrlImagen = slider.UrlImagen;
+
+            _context.SaveChanges();
+        }
+    }
+}
