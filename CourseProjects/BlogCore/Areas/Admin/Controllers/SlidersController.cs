24c4aa2037f26fca78bd77d7db0a230d859e8c76
D-Madev <3matias.sm@gmail.com>
2025-09-24 08:26:45 -0300

    feat(Controllers-Sliders): Agrego funcionalidad getAll() y nombre de archivo standard

---
+++ CourseProjects/BlogCore/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,28 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    public class SlidersController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public SlidersController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Llamadas a la API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Slider.GetAll() });
+        }
+        #endregion
+    }
+}
